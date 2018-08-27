003b3a4c6712143ec3a370894bb6a880737ca14b
zaiyangliu <lzyxn@126.com>
2018-08-27 09:08:01 +0800

    a

---
+++ 101/101.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+  int num;
+  scanf("%d", &num);
+  double distance;
+  while(num--){
+    float x1, y1, x2, y2;
+    scanf("%f %f %f %f", &x1, &y1, &x2, &y2);
+    distance = sqrt( ( ( (x1 - x2) )*(x1 - x2) + ( ( y1 - y2))*( y1 - y2) ) );
+    printf("%.2f\n", distance);
+  }
+  return 0;
+}
+    
+		   
